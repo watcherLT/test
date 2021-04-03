a3fb8e2ea573a3a81e9f224244aaa43ee1bc8876
luotao <luotao@windows10.microdone.cn>
2021-04-03 18:16:46 +0800

    我修改了

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("fuck!");
+		System.out.println("fuck! 那现在看得到我吗");
 	}
 
 }
